9efe440a9aaabdfb2606e46aac22d4443209512d
lksprado <lks-prado@live.com>
2026-09-13 18:51:40 -0300

    ref: staging nhl hockeyfights e players melhorada

--- models/staging/nhl/stg_all_hockeyfights.sql
+++ models/staging/nhl/stg_all_hockeyfights.sql
@@ -1,10 +1,7 @@
 {{
   config(
     materialized = 'table',
-    tags = ['nhl','staging', 'player_id'],
-    post_hook = [
-        "create index if not exists idx_fights on {{ this }} (fight_id)"
-    ]
+    tags = ['nhl'],
     )
 }}
 
@@ -20,18 +17,17 @@ renamed AS (
             WHEN season_type LIKE 'reg' THEN 2
             WHEN season_type LIKE 'pos' THEN 3
         END                                                                   AS game_type_id,
-        {{ dbt_utils.generate_surrogate_key(['fight', 'date', 'gametime']) }} AS fight_id,
-        fight                                                                 AS fight_desc,
-        player_1_name,
-        player_2_name,
-        player_1_team,
-        player_2_team,
-        SPLIT_PART(SPLIT_PART(fight, '(', 2), ')', 1)                         AS team_1_code,
-        SPLIT_PART(SPLIT_PART(fight, '(', 3), ')', 1)                         AS team_2_code,
+        LOWER(fight)                                                          AS description,
+        REPLACE(LOWER(player_1_name),',', ' ')                                AS player_1_abbrev_name,
+        REPLACE(LOWER(player_2_name),',', ' ')                                AS player_2_abbrev_name,
+        LOWER(player_1_team)                                                  AS player_1_team_full_name,
+        LOWER(player_2_team)                                                  AS player_2_team_full_name,
+        SPLIT_PART(SPLIT_PART(fight, '(', 2), ')', 1)                         AS team_1_abbrev_name,
+        SPLIT_PART(SPLIT_PART(fight, '(', 3), ')', 1)                         AS team_2_abbrev_name,
         TO_DATE(date, 'MM/DD/YY')                                             AS game_date,
-        period,
+        LOWER(period)                                                         AS period,
         gametime                                                              AS time_in_period,
-        winner                                                                AS fight_winner,
+        LOWER(winner)                                                         AS fight_winner_full_name,
         rating::FLOAT                                                         AS rating,
         vote_count::INT                                                       AS vote_count
     FROM source
@@ -40,40 +36,39 @@ renamed AS (
 dedup AS (
     SELECT
         *,
-        ROW_NUMBER() OVER (PARTITION BY fight_id) AS rn
+        ROW_NUMBER() OVER (PARTITION BY description, game_date, time_in_period) AS rn
     FROM renamed
 ),
 
 final AS (
     SELECT
-        fight_id,
-        fight_desc,
         season_id,
         game_type_id,
-        player_1_name,
-        player_2_name,
-        player_1_team,
-        player_2_team,
+        description,
+        player_1_abbrev_name,
+        player_2_abbrev_name,
+        player_1_team_full_name,
+        player_2_team_full_name,
         game_date,
         period,
         time_in_period,
-        fight_winner,
+        fight_winner_full_name,
         rating,
         vote_count,
         CASE
-            WHEN team_1_code LIKE 'MON' THEN 'MTL'
-            WHEN team_1_code LIKE 'WAS' THEN 'WSH'
-            WHEN team_1_code LIKE 'CAL' THEN 'CGY'
-            ELSE team_1_code
-        END AS team_1_code,
+            WHEN team_1_abbrev_name LIKE 'MON' THEN 'MTL'
+            WHEN team_1_abbrev_name LIKE 'WAS' THEN 'WSH'
+            WHEN team_1_abbrev_name LIKE 'CAL' THEN 'CGY'
+            ELSE team_1_abbrev_name
+        END AS team_1_abbrev_name,
         CASE
-            WHEN team_2_code LIKE 'MON' THEN 'MTL'
-            WHEN team_2_code LIKE 'WAS' THEN 'WSH'
-            WHEN team_2_code LIKE 'CAL' THEN 'CGY'
-            ELSE team_2_code
-        END AS team_2_code
+            WHEN team_2_abbrev_name LIKE 'MON' THEN 'MTL'
+            WHEN team_2_abbrev_name LIKE 'WAS' THEN 'WSH'
+            WHEN team_2_abbrev_name LIKE 'CAL' THEN 'CGY'
+            ELSE team_2_abbrev_name
+        END AS team_2_abbrev_name
     FROM dedup
     WHERE rn = 1
 )
 
-SELECT * FROM final
+SELECT * FROM final ORDER BY game_date DESC
--- models/staging/nhl/stg_all_players.sql
+++ models/staging/nhl/stg_all_players.sql
@@ -1,23 +1,19 @@
 {{
   config(
     materialized = 'table',
-    tags = ['nhl','staging', 'player_id'],
+    tags = ['nhl','staging', 'id'],
     post_hook = [
-        "create index if not exists idx_players_id on {{ this }} (player_id)"
+        "create index if not exists idx_players_id on {{ this }} (id)"
     ]
     )
 }}
 
 WITH
 source AS (
-    SELECT * FROM {{ source('nhl', 'nhl_raw_all_players') }}
-),
-
-regular AS (
     SELECT
-        (payload ->> 'playerId')::INT                                                  AS player_id,
-        (payload -> 'firstName' ->> 'default')                                         AS player_firstname,
-        (payload -> 'lastName' ->> 'default')                                          AS player_lastname,
+        (payload ->> 'playerId')::INT                                                  AS id,
+        (payload -> 'firstName' ->> 'default')                                         AS first_name,
+        (payload -> 'lastName' ->> 'default')                                          AS last_name,
         (payload ->> 'isActive')::BOOLEAN                                              AS is_active,
         (payload ->> 'position')                                                       AS position,
         (payload -> 'birthCity' ->> 'default')                                         AS birth_city,
@@ -35,16 +31,11 @@ regular AS (
         (payload -> 'careerTotals' -> 'regularSeason' ->> 'gamesPlayed')::INT          AS games_played,
         (payload -> 'careerTotals' -> 'regularSeason' ->> 'shootingPctg')::FLOAT       AS shooting_pctg,
         (payload -> 'careerTotals' -> 'regularSeason' ->> 'powerPlayGoals')::INT       AS powerplay_goals,
-        (payload -> 'careerTotals' -> 'regularSeason' ->> 'powerPlayPoints')::INT
-            AS powerplay_points,
-        (payload -> 'careerTotals' -> 'regularSeason' ->> 'gameWinningGoals')::INT
-            AS game_winning_goals,
-        (payload -> 'careerTotals' -> 'regularSeason' ->> 'shorthandedGoals')::INT
-            AS shorthanded_goals,
-        (payload -> 'careerTotals' -> 'regularSeason' ->> 'shorthandedPoints')::INT
-            AS shorthanded_points,
-        (payload -> 'careerTotals' -> 'regularSeason' ->> 'faceoffWinningPctg')::FLOAT
-            AS faceoff_win_pctg,
+        (payload -> 'careerTotals' -> 'regularSeason' ->> 'powerPlayPoints')::INT      AS powerplay_points,
+        (payload -> 'careerTotals' -> 'regularSeason' ->> 'gameWinningGoals')::INT     AS game_winning_goals,
+        (payload -> 'careerTotals' -> 'regularSeason' ->> 'shorthandedGoals')::INt     AS shorthanded_goals,
+        (payload -> 'careerTotals' -> 'regularSeason' ->> 'shorthandedPoints')::INT    AS shorthanded_points,
+        (payload -> 'careerTotals' -> 'regularSeason' ->> 'faceoffWinningPctg')::FLOAT AS faceoff_win_pctg,
         (payload -> 'draftDetails' ->> 'year')::INT                                    AS draft_year,
         (payload -> 'draftDetails' ->> 'round')::INT                                   AS draft_round,
         (payload -> 'draftDetails' ->> 'teamAbbrev')                                   AS draft_team_id,
@@ -57,69 +48,56 @@ regular AS (
         (payload ->> 'heightInInches')::INT                                            AS height_inches,
         (payload ->> 'weightInPounds')::INT                                            AS weight_pounds,
         (payload ->> 'heightInCentimeters')::INT                                       AS height_centimeters,
-        (payload ->> 'weightInKilograms')::INT                                         AS weight_kilogram,
-        'regular'                                                                      AS season_type
-    FROM source
+        (payload ->> 'weightInKilograms')::INT                                         AS weight_kilogram
+    FROM {{ source('nhl', 'nhl_raw_all_players') }}
     WHERE
         payload IS NOT NULL
         AND payload <> 'null'
+    
 ),
-
-playoffs AS (
-    SELECT
-        (payload ->> 'playerId')::INT                                             AS player_id,
-        (payload -> 'firstName' ->> 'default')                                    AS player_firstname,
-        (payload -> 'lastName' ->> 'default')                                     AS player_lastname,
-        (payload ->> 'isActive')::BOOLEAN                                         AS is_active,
-        (payload ->> 'position')                                                  AS position,
-        (payload -> 'birthCity' ->> 'default')                                    AS birth_city,
-        (payload ->> 'birthDate')::DATE                                           AS birthdate,
-        (payload ->> 'birthCountry')                                              AS birth_country,
-        (payload -> 'birthStateProvince' ->> 'default')                           AS birth_state_province,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'pim')::INT                  AS pim,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'goals')::INT                AS goals,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'shots')::INT                AS shots,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'avgToi')                    AS avg_toi,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'points')::INT               AS points,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'assists')::INT              AS assists,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'otGoals')::INT              AS overtime_goals,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'plusMinus')::INT            AS plus_minus,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'gamesPlayed')::INT          AS games_played,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'shootingPctg')::FLOAT       AS shooting_pctg,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'powerPlayGoals')::INT       AS powerplay_goals,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'powerPlayPoints')::INT      AS powerplay_points,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'gameWinningGoals')::INT     AS game_winning_goals,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'shorthandedGoals')::INT     AS shorthanded_goals,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'shorthandedPoints')::INT
-            AS shorthanded_points,
-        (payload -> 'careerTotals' -> 'playoffs' ->> 'faceoffWinningPctg')::FLOAT
-            AS faceoff_win_pctg,
-        (payload -> 'draftDetails' ->> 'year')::INT                               AS draft_year,
-        (payload -> 'draftDetails' ->> 'round')::INT                              AS draft_round,
-        (payload -> 'draftDetails' ->> 'teamAbbrev')                              AS draft_team_id,
-        (payload -> 'draftDetails' ->> 'overallPick')::INT                        AS draft_overall_pick,
-        (payload -> 'draftDetails' ->> 'pickInRound')::INT                        AS draft_pick_in_round,
-        (payload -> 'fullTeamName' ->> 'default')                                 AS team_full_name,
-        (payload ->> 'currentTeamId')::INT                                        AS current_team_id,
-        (payload ->> 'shootsCatches')                                             AS shoots_catches,
-        (payload ->> 'sweaterNumber')::INT                                        AS sweater_number,
-        (payload ->> 'heightInInches')::INT                                       AS height_inches,
-        (payload ->> 'weightInPounds')::INT                                       AS weight_pounds,
-        (payload ->> 'heightInCentimeters')::INT                                  AS height_centimeters,
-        (payload ->> 'weightInKilograms')::INT                                    AS weight_kilogram,
-        'playoffs'                                                                AS season_type
+renamed AS (
+    SELECT 
+        id,
+        CONCAT(LOWER(first_name), ' ', LOWER(last_name)) AS full_name,
+        LOWER(first_name) AS first_name,
+        LOWER(last_name) AS last_name,
+        is_active,
+        position,
+        LOWER(birth_city) AS birth_city,
+        birthdate,
+        birth_country,
+        LOWER(birth_state_province) AS birth_state_province,
+        pim,
+        goals,
+        shots,
+        avg_toi,
+        points,
+        assists,
+        overtime_goals,
+        plus_minus,
+        games_played,
+        shooting_pctg,
+        powerplay_goals,
+        powerplay_points,
+        game_winning_goals,
+        shorthanded_goals,
+        shorthanded_points,
+        faceoff_win_pctg,
+        draft_year,
+        draft_round,
+        draft_team_id AS draft_team_abbrev_name,
+        draft_overall_pick,
+        draft_pick_in_round,
+        LOWER(team_full_name) AS team_full_name,
+        current_team_id,
+        shoots_catches,
+        sweater_number,
+        height_inches,
+        weight_pounds,
+        height_centimeters,
+        weight_kilogram
     FROM source
-    WHERE
-        payload IS NOT NULL
-        AND payload <> 'null'
-),
-
-union_tbs AS (
-    SELECT * FROM regular
-    UNION ALL
-    SELECT * FROM playoffs
 )
-
-SELECT * FROM union_tbs
-WHERE player_id IS NOT NULL
-ORDER BY player_id DESC, season_type DESC
+SELECT * FROM renamed
+WHERE id IS NOT NULL
+ORDER BY id DESC
