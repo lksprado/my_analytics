a678e0acb81bb4a0927c6fac1d6b9249c485cb00
lksprado <lks-prado@live.com>
2026-09-14 13:27:11 -0300

    fix: bugs

--- models/staging/b3/stg_proventos.sql
+++ models/staging/b3/stg_proventos.sql
@@ -37,7 +37,7 @@ renamed AS (
         {{ clean_string("produto", "upper") }}        AS produto,
         {{ clean_string("instituicao", "upper") }}    AS instituicao,
         {{ clean_string("tipo_de_evento", "upper") }} AS tipo_provento,
-        quantidade::INT                               AS quantidade,
+        quantidade::NUMERIC::INT                      AS quantidade,
         preco_unitario,
         valor_liquido::NUMERIC(18, 2)                 AS vlr_liquido_brl,
         'BRL'                                         AS moeda_ativo
